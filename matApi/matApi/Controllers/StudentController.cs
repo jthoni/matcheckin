4939f1c20971aa3ba26dca6eae7a461845fb8eb2
jthoni <jthoni@hotmail.com>
2015-09-10 20:50:22 -0700

    Add api project and a basic view

---
+++ matApi/matApi/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using matApi.Models;
+
+namespace matApi.Controllers
+{
+    [Route("api/Student/GetAllStudents")]
+    public class StudentController : ApiController
+    {
+        private StudentDirectory2015Entities studenDb = new StudentDirectory2015Entities();
+
+        public IQueryable<StudentList> GetAllStudents()
+        {
+            var results = studenDb.StudentLists;
+            return results;
+        }
+        
+    }
+}
