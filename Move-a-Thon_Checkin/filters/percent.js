bc20bd9d99c1fddadb4303c5be96dffb7fccd714
jthoni <jthoni@hotmail.com>
2015-09-27 17:55:16 -0700

    final

---
+++ Move-a-Thon_Checkin/filters/percent.js
@@ -0,0 +1,6 @@
+﻿// Filter to display a value as a percentage
+matApp.filter('percent', function () {
+    return function (input) {
+        return (parseFloat(input) * 100).toFixed(1) + "%";
+    };
+});
